2187ea74211dc1e915896c2bf8f66027927d9ddf
USER <USER@DESKTOP-FFJMR13>
2022-03-21 15:46:37 +0530

    save java program

---
+++ Example14/src/com/Basic/AdditionExmp.java
@@ -0,0 +1,13 @@
+package com.Basic;
+
+public class AdditionExmp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 10,b= 20, c;
+		c = i+b;
+		System.out.println(c);
+
+	}
+
+}
