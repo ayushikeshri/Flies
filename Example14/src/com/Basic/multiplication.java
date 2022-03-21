59f557ef45e05b862a5824923bd7d7e07dabffd1
USER <USER@DESKTOP-FFJMR13>
2022-03-21 16:02:27 +0530

    java program

---
+++ Example14/src/com/Basic/multiplication.java
@@ -0,0 +1,12 @@
+package com.Basic;
+
+public class multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int a = 9, b =9 ,c;
+   c = a*b;
+   System.out.println(c);
+	}
+
+}
